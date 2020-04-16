2e636c41b82dea9fc1d3c4fbc4d123fa51d85d15
RusTaylor <rustaylor.web@gmail.com>
2020-04-16 12:34:08 +0300

    require bootstrap into head template

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,4 +1,8 @@
 <head>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.css">
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
